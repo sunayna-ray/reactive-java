73b9c79d3f4c1b358ad8ab7edda4b971b32a422c
sunayna-ray <sunayna0501@gmail.com>
2023-07-12 12:09:01 -0700

    Stream practice

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
@@ -7,23 +10,32 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(num -> num<5)
+                .forEach(num->System.out.println(num));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(num-> num<5).skip(1)
+                .limit(2)
+                .forEach(num->System.out.println(num));
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(num->num>5)
+                .findFirst().orElse(-1);
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        StreamSources.userStream().forEach(user -> System.out.println(user.getFirstName()));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .forEach(user -> System.out.println(user.getFirstName()));
 
     }
 
